0ce19cab6717b36cc7b2854c2282bf6310cb680b
abouafia <amir.bouafia@etu.unistra.fr>
2021-04-05 18:31:42 +0200

    comments add

--- src/board.cpp
+++ src/board.cpp
@@ -37,7 +37,9 @@ void Board::draw(SDL_Renderer *renderer_, std::queue<int>randomTetromino_)
   SDL_RenderDrawRect(renderer_, &rect_);
 
   /***** Draw the spawn window *****/
+  // The left window for the current tetromino
   
+  // Draw the window
   SDL_SetRenderDrawColor(renderer_, fillColor_.r, fillColor_.g, fillColor_.b,
                          fillColor_.a);
 
@@ -47,6 +49,9 @@ void Board::draw(SDL_Renderer *renderer_, std::queue<int>randomTetromino_)
   rect2_.h = 200;
   SDL_RenderFillRect(renderer_, &rect2_);
 
+  // Draw the lines for the grid
+  // Value 50 is the size of the cell for the left window maybe
+  //--> maybe create a variable CELL to use and modify for each rectangle
   SDL_SetRenderDrawColor(renderer_, borderColor_.r, borderColor_.g,
                          borderColor_.b, borderColor_.a);
   SDL_RenderDrawRect(renderer_, &rect2_);
@@ -66,12 +71,12 @@ void Board::draw(SDL_Renderer *renderer_, std::queue<int>randomTetromino_)
   }
 
   /***** Draw the spawn window for the next fourth tetromino *****/
-
+  // Draw the window
   SDL_SetRenderDrawColor(renderer_, fillColor_.r, fillColor_.g, fillColor_.b,
                          fillColor_.a);
   rect3_ = rect_;
   rect3_.x += rect_.w + 50;
-  rect3_.w = 192;
+  rect3_.w = 192; //initially 160 but 192 fit more cause 6 col --> better renderer
   SDL_RenderFillRect(renderer_, &rect3_);
 
   SDL_SetRenderDrawColor(renderer_, borderColor_.r, borderColor_.g,
@@ -91,26 +96,31 @@ void Board::draw(SDL_Renderer *renderer_, std::queue<int>randomTetromino_)
     SDL_RenderDrawLine(renderer_, rect3_.x, y, rect3_.x + rect3_.w, y);
   }
   
+  //Tmp is use here to draw the tetromino
+  //We won't modify the original queue here
   std::queue<int> tmp = randomTetromino_;
-  //To jump a line between tetro spawners
+
+  //To jump the fist line to have a spacing with the border for the right window
   int spaceLine = 64;
-  //To do not erase the x and y
+
+  //To do not erase the x and y because at each iteration it's modifying
   int tmpx, tmpy;
   tmpx = rect2_.x;
   tmpy = rect2_.y;
 
-  //First spawner
+  //First tetromino -> left window
   drawTetrominoSpawn(renderer_, tmp.front(),true,spaceLine,tmpx,tmpy);
   tmp.pop();
 
-  //To do not erase the x and y
+  //To do not erase the x and y because at each iteration it's modifying
   tmpx = rect3_.x;
   tmpy = rect3_.y;
-  //Fourth others spawners
+  //Fourth others tetromino -> right winodw
   while (!tmp.empty())
   {
     drawTetrominoSpawn(renderer_, tmp.front(),false, spaceLine,tmpx,tmpy);
     tmp.pop();
+    //To jump 2 lines between 2 tetrominos
     spaceLine += 128;
   }
   
@@ -119,12 +129,18 @@ void Board::draw(SDL_Renderer *renderer_, std::queue<int>randomTetromino_)
 
 void Board::drawTetrominoSpawn(SDL_Renderer *renderer_, int nbTypeTetro, bool b, int spaceLine, int tmpx, int tmpy)
 {
+  //  4 is the size of the matrix of the structure tetro
+  // --> we could replace 4 by the size
   for (int i = 0; i < 4; i++)
   {
     for (int j = 0; j < 4; j++)
     {
+      //If 1 we draw a square
       if (tab[nbTypeTetro].matrix[i][j] == 1)
       {
+        // boolean to know if we are on the left or right window
+        // for height and width we do -1 to know draw on the golden lines of the grid
+        // for the y : 50 is the cell size and 51 is to jump of one row to center the tetromino
         if(b == true)
         {
           rect2_.x = tmpx;
@@ -133,7 +149,8 @@ void Board::drawTetrominoSpawn(SDL_Renderer *renderer_, int nbTypeTetro, bool b,
           rect2_.w = 50 - 1;
           rect2_.h = 50 - 1;
 
-          // To center the square
+          // To center the type one of tetromino : the yellow square
+          // for the x : 50 is the cell size and 51 is to shift of one column to center the tetromino
           if(nbTypeTetro == 1)
           {
             rect2_.x = rect2_.x + j * 50 + 51;
@@ -143,6 +160,8 @@ void Board::drawTetrominoSpawn(SDL_Renderer *renderer_, int nbTypeTetro, bool b,
             rect2_.x = rect2_.x + j * 50 + 1;
           }
         }
+        // right window, 32 is the size of cell
+        // it's the same thing just we add spaceLine for the y to jump more rows
         else
         {
           rect3_.x = tmpx + 32;
