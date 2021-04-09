46535354736c4e3e856b6aed4b2608130fc5eec2
abouafia <amir.bouafia@etu.unistra.fr>
2021-04-09 19:42:26 +0200

    begin write tetromino

---
+++ src/tetromino.cpp
@@ -0,0 +1,16 @@
+#include "tetromino.hpp"
+/*
+std::queue<int> Tetromino::randomTetromino()
+{
+  int random;
+  srand(time(NULL));
+  // Fill the queue to always see the next tetromino + actual tetromino
+  while(randomTetromino_.size() < 5)
+  {
+    std::cout << "IM IN" << std::endl;
+    random = rand() % 7;
+    std::cout << random << std::endl;
+    randomTetromino_.push(random);
+  }  
+  return randomTetromino_;
+}*/
--- src/tetromino.hpp
+++ src/tetromino.hpp
@@ -2,16 +2,87 @@
 #define TETROMINO_HPP
 
 #include <SDL2/SDL.h>
+#include <utility>
+#include <queue>
+#include <iostream>
 
 class Tetromino
 {
 private:
+    struct tetromino
+    {
+        int matrix[4][4];
+        SDL_Color tetrominoColor;
+        std::pair<int,int> pos;
+    };
+
+    tetromino tab[7] =
+    {
+        {
+            {{1,1,1,1}
+            ,{0,0,0,0}
+            ,{0,0,0,0}
+            ,{0,0,0,0}},
+            {0x00,0xFF,0xFF,0xFF},
+            {0,0},
+        },
+        {
+            {{1,1,0,0}
+            ,{1,1,0,0}
+            ,{0,0,0,0}
+            ,{0,0,0,0}},
+            {0xFF,0xFF,0x0,0xFF},
+            {0,0},
+        },
+        {
+            {{1,1,1,0}
+            ,{0,1,0,0}
+            ,{0,0,0,0}
+            ,{0,0,0,0}},
+            {0x8B,0x00,0x8B,0xFF},
+            {0,0},
+        },
+        {
+            {{1,1,1,0}
+            ,{1,0,0,0}
+            ,{0,0,0,0}
+            ,{0,0,0,0}},
+            {0xFF,0x8C,0x00,0xFF},
+            {0,0},
+        },
+        {
+            {{1,1,1,0}
+            ,{0,0,1,0}
+            ,{0,0,0,0}
+            ,{0,0,0,0}},
+            {0x00,0x00,0xFF,0xFF},
+            {0,0},
+        },
+        {
+            {{1,1,0,0}
+            ,{0,1,1,0}
+            ,{0,0,0,0}
+            ,{0,0,0,0}},
+            {0xFF,0x00,0x00,0xFF},
+            {0,0},
+        },
+        {
+            {{0,1,1,0}
+            ,{1,1,0,0}
+            ,{0,0,0,0}
+            ,{0,0,0,0}},
+            {0x00,0xFF,0x00,0xFF},
+            {0,0}
+        }
+    };
 
 public:
 
-    void randomTretromino();
+    std::queue<int> randomTetromino_;
+
+    std::queue<int> randomTretromino();
     void randomPosition();
 
-}
+};
 
 #endif // TETROMINO_HPP
